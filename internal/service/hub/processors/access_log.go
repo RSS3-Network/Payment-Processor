fb212d5946bdad653e1efbc4d1a55d7f3e797621
Nya Candy <dev@candinya.com>
2024-04-12 11:52:12 +0800

    fix: lint

--- internal/service/hub/processors/access_log.go
+++ internal/service/hub/processors/access_log.go
@@ -2,6 +2,7 @@ package processors
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -12,7 +13,7 @@ import (
 )
 
 func (app *App) ProcessAccessLog(l *accesslog.Log) {
-	rctx := context.Background()
+	ctx := context.Background()
 
 	zap.L().Debug("new access log arrive")
 
@@ -21,71 +22,85 @@ func (app *App) ProcessAccessLog(l *accesslog.Log) {
 		return
 	}
 
-	// Find user
-	keyIDParsed, err := strconv.ParseUint(*l.KeyID, 10, 64)
-
+	// Find account and key
+	key, account, err := app.findAccount(ctx, *l.KeyID)
 	if err != nil {
-		zap.L().Error("recover key id", zap.String("keyID string", *l.KeyID), zap.Error(err))
+		zap.L().Error("find account", zap.Error(err))
 		return
 	}
 
-	key, _, err := model.KeyGetByID(rctx, keyIDParsed, false, app.databaseClient, app.controlClient) // Deleted key could also be used for pending bills
-
-	if err != nil {
-		zap.L().Error("get key by id", zap.Uint64("keyID", keyIDParsed), zap.Error(err))
-		return
+	// Update consumption
+	if err = app.updateConsumption(ctx, key, l.Status == http.StatusOK && !key.Account.IsPaused); err != nil {
+		zap.L().Error("update consumption", zap.Error(err))
 	}
 
-	user, err := key.GetAccount(rctx)
+	// Pause account
+	if err = app.pauseAccount(ctx, account); err != nil {
+		zap.L().Error("pause account", zap.Error(err))
+	}
+}
 
+func (app *App) findAccount(ctx context.Context, keyID string) (*model.Key, *model.Account, error) {
+	keyIDParsed, err := strconv.ParseUint(keyID, 10, 64)
+	if err != nil {
+		return nil, nil, fmt.Errorf("recover key id (%s): %w", keyID, err)
+	}
+
+	key, _, err := model.KeyGetByID(ctx, keyIDParsed, false, app.databaseClient, app.controlClient) // Deleted key could also be used for pending bills
+	if err != nil {
+		return nil, nil, fmt.Errorf("get key by id (%s): %w", keyID, err)
+	}
+
+	user, err := key.GetAccount(ctx)
 	if err != nil {
 		// Failed to get account
-		zap.L().Error("get account", zap.Error(err))
-		return
+		return nil, nil, fmt.Errorf("get account (%s): %w", keyID, err)
 	}
 
-	if l.Status != http.StatusOK || key.Account.IsPaused {
-		// Request failed or is in free tier, only increase API call count
-		if err = key.ConsumeRu(rctx, 0); err != nil {
-			// Failed to consumer RU
-			zap.L().Error("increase API call count", zap.Any("account", user), zap.Any("key", key), zap.Error(err))
+	return key, user, nil
+}
+
+func (app *App) updateConsumption(ctx context.Context, key *model.Key, isRUConsumption bool) error {
+	// Request failed or is in free tier, only increase API call count
+	if err := key.ConsumeRu(ctx, 0); err != nil {
+		// Failed to consumer RU
+		return fmt.Errorf("key (%v) increase API call count: %w", key, err)
+	}
+
+	if isRUConsumption {
+		ru := int64(1) // Default // TODO
+
+		if err := key.ConsumeRu(ctx, ru); err != nil {
+			// Failed to consume RU
+			return fmt.Errorf("key (%v) consume RU: %w", key, err)
 		}
-
-		return
 	}
 
-	// Consumer RU
-	ru := int64(1) // Default // TODO
+	return nil
+}
 
-	if err = key.ConsumeRu(rctx, ru); err != nil {
-		// Failed to consume RU
-		zap.L().Error("consume RU", zap.Any("account", user), zap.Any("key", key), zap.Error(err))
-
-		return
-	}
-
-	ruRemain, err := user.GetBalance(rctx)
-
+func (app *App) pauseAccount(ctx context.Context, account *model.Account) error {
+	ruRemain, err := account.GetBalance(ctx)
 	if err != nil {
 		// Failed to get remain RU
-		zap.L().Error("get account remain RU", zap.Any("account", user), zap.Error(err))
-
-		return
+		return fmt.Errorf("get account (%v) remain RU: %w", account, err)
 	}
 
 	if ruRemain < 0 {
-		zap.L().Info("Insufficient remain RU, pause account", zap.Any("account", user))
-		// Pause user account
-		if !user.IsPaused {
-			if err = app.controlClient.PauseAccount(rctx, user.Address.Hex()); err != nil {
-				zap.L().Error("pause account in control", zap.Any("account", user), zap.Error(err))
-			} else if err = app.databaseClient.WithContext(rctx).
+		zap.L().Info("Insufficient remain RU, pause account", zap.Any("account", account))
+		// Pause account
+		if !account.IsPaused {
+			if err = app.controlClient.PauseAccount(ctx, account.Address.Hex()); err != nil {
+				return fmt.Errorf("pause account (%v) in control: %w", account, err)
+			} else if err = app.databaseClient.WithContext(ctx).
 				Model(&table.GatewayAccount{}).
-				Where("address = ?", user.Address).
+				Where("address = ?", account.Address).
 				Update("is_paused", true).
 				Error; err != nil {
-				zap.L().Error("save paused account into db", zap.Any("account", user), zap.Error(err))
+				return fmt.Errorf("save paused account (%v) in into db: %w", account, err)
 			}
 		}
 	}
+
+	return nil
 }
